91305a494e0ecc5a5eb461e367965c3f8c2894bc
AltynaiUrmatbekova <64131999+AltynaiUrmatbekova@users.noreply.github.com>
2022-02-21 15:58:57 +0600

    added new script

---
+++ script.js
@@ -0,0 +1,21 @@
+let h1 = document.querySelector('h1');
+
+document.querySelector('#text').addEventListener('input', function() {
+    h1.textContent = this.value;
+});
+
+document.querySelector('#color').addEventListener('input', function() {
+    h1.style.color = this.value;
+});
+
+document.querySelector('#background-color').addEventListener('input', function() {
+    h1.style.backgroundColor = this.value;
+});
+
+document.querySelector('#font-size').addEventListener('input', function() {
+    h1.style.fontFamily = this.value;
+});
+
+document.querySelector('font-family').addEventListener('input', function() {
+    h1.style.fontFamily = this.value;
+})
